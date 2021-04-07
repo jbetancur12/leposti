dc20c545b4fa46e871fda1fbcf29f56fac1e7bfd
Jorge Betancur <jorgebetancur@Jorges-MacBook-Air.local>
2021-04-06 23:18:15 -0500

    Ended

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,33 +1,50 @@
-import React from 'react'
-import moment from 'moment';
-import { Row, Col, Select, Form, DatePicker, Space } from 'antd';
-import ColumnGroup from 'antd/lib/table/ColumnGroup';
+import React from "react";
+import moment from "moment";
+import dynamic from "next/dynamic";
+import { Row, Col, Select, Form, DatePicker, Space, Checkbox, Button, Input } from "antd";
+import ColumnGroup from "antd/lib/table/ColumnGroup";
 
 const { Option } = Select;
-const FormItem = Form.Item
+const FormItem = Form.Item;
 const { RangePicker } = DatePicker;
 
-const dateFormat = 'DD/MM/YYYY';
+const dateFormat = "DD/MM/YYYY";
+
+const QuillNoSSRWrapper = dynamic(import("react-quill"), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
 
 export default function Index({ products }) {
-  const [product, setProduct] = React.useState('');
-  const [provider, setProvider] = React.useState('');
+
+  const myRef = React.useRef(null);
+  const [product, setProduct] = React.useState("");
+  const [provider, setProvider] = React.useState("");
   const [open, setOpen] = React.useState(false);
   const [openProviders, setOpenProviders] = React.useState(false);
   const [productProvider, setProductProvider] = React.useState([]);
   const [providers, setProviders] = React.useState([]);
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   const [readOnly, setReadOnly] = React.useState(true);
+  const [valueEditor, setValueEditor] = React.useState("");
+  const [valueEditorText, setValueEditorText] = React.useState("");
+  const [terms, setTerms] = React.useState(false)
 
+ 
 
+  const config = {
+    theme: "snow",
+    modules: {
+      toolbar: false,
+      clipboard: {},
+    },
+  };
   async function onChange(value) {
-    const res = await fetch(
-      `https://api.leposti.ml/products/${value}`,
-    );
+    const res = await fetch(`https://api.leposti.ml/products/${value}`);
     const resProduct = await res.json();
     const _product = { product: value };
     setProduct(value);
-    setProvider('');
+    setProvider("");
     setProductProvider({ ..._product });
     setProviders(resProduct.providers);
   }
@@ -38,13 +55,29 @@ export default function Index({ products }) {
     setProductProvider(test);
   }
 
-  function onChangeDate() {
-    console.log('focus');
+  function onChangeEditor(content, delta, source, editor) {
+    console.log(source);
+    setValueEditor(editor.getHTML());
+    setValueEditorText(editor.getText());
   }
 
-  function disabledDate(current){
-    return current && current < moment().endOf('day');
+  function onChangeDate() {
+    setReadOnly(false);
   }
+
+  function onChangeTerms(e) {
+    setTerms(e.target.checked)
+  }
+
+  function disabledDate(current) {
+    return current && current < moment().endOf("day");
+  }
+
+
+
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
 
   const optionsProducts = products.map((product) => {
     return (
@@ -63,12 +96,11 @@ export default function Index({ products }) {
       );
     });
 
-
   return (
     <Row justify="space-around" style={{ marginTop: "50px" }}>
       <Col span={16}>COntent</Col>
       <Col span={8}>
-        <Form layout="horizontal">
+        <Form layout="vertical" onFinish={onFinish}>
           <FormItem
             label="Producto:"
             labelCol={{ span: 8 }}
@@ -113,17 +145,62 @@ export default function Index({ products }) {
             label="Fecha:"
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 8 }}
+            rules={[{
+              required: true,
+              type:"date",
+              message:"Date"
+            }]}
           >
             <Space direction="vertical">
-              <DatePicker disabledDate={disabledDate} disabled={!productProvider.provider} defaultValue={moment(new Date(), dateFormat)} format={dateFormat} onChange={onChangeDate} />
+              <DatePicker
+                disabledDate={disabledDate}
+                disabled={!productProvider.provider}
+                defaultValue={moment(new Date(), dateFormat)}
+                format={dateFormat}
+                onChange={onChangeDate}
+              />
             </Space>
           </FormItem>
-
+          <FormItem
+            label="Contenido:"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 20 }}
+            rules={[{
+              required: true,
+            }]}
+            
+          >
+            <QuillNoSSRWrapper
+              ref={myRef}
+              onChange={onChangeEditor}
+              theme="snow"
+              modules={config.modules}
+              value={valueEditor}
+              readOnly={readOnly}
+              placeholder= "Contenido"
+            />
+          </FormItem>
+  
+        <FormItem label="Email:"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 20 }}name="email" rules={[{
+              required: true,
+              type: "email",
+              message: "The input is not valid E-mail!"
+            }]}>
+        <Input placeholder="Email"></Input>
+      </FormItem>
+          <FormItem labelCol={{ span: 8 }} wrapperCol={{ span: 24 }}>
+            <Checkbox onChange={onChangeTerms}>Acepto Terminos y condiciones</Checkbox>
+          </FormItem>
+          <FormItem labelCol={{ span: 8 }} wrapperCol={{ span: 24 }}>
+          <Button type="primary"  htmlType="submit" disabled={!terms}>Cotizar</Button>
+          </FormItem>
         </Form>
       </Col>
     </Row>
-  )
-};
+  );
+}
 Index.getInitialProps = async (ctx) => {
   const res = await fetch(`https://api.leposti.ml/products`);
   const products = await res.json();
