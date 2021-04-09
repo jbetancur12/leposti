3d012ce596c8e734eac4fb98fbd2c5ba46ddf298
Jorge Betancur <jabetancur12@gmail.com>
2021-04-09 18:52:51 -0500

    Hola

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -270,7 +270,7 @@ export default function Index({ products }) {
 
       <Row justify="space-around" style={{ marginTop: "50px" }}>
         <Col span={16}>
-          {" HOLAAAA1"}
+          {" HOLAAAA2"}
           <main className={styles.main}>
             <h1 className={styles.title}>
               Welcome to <a href="https://nextjs.org">Next.js!</a>
