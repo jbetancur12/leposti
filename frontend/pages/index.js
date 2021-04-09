27e41e68c31ec85273e7abea547bb519f62b6fd9
Jorge Betancur <jabetancur12@gmail.com>
2021-04-09 09:15:16 -0500

    webhokk7

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -62,7 +62,7 @@ export default function Index({ products }) {
     },
   };
   async function onChange(value) {
-    console.log("Token-webhook-6",process.env.NEXT_PUBLIC_JWT_TOKEN)
+    console.log("Token-webhook-7",process.env.NEXT_PUBLIC_JWT_TOKEN)
     const res = await fetch(`https://api.leposti.ml/products/${value}`, {
       headers: {
         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE3OTM5NzA2LCJleHAiOjE2MjA1MzE3MDZ9.lwwNZWcqvDCkmzxKHWaglDtYjkFTizqD5s_0oXEHcgQ`,
