8141192e0b93a8617ab1fca12f6402324a80a2d8
Jorge Betancur <jabetancur12@gmail.com>
2021-04-09 02:15:13 -0500

    Test webhooks 2

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -69,7 +69,7 @@ export default function Index({ products }) {
         "Content-Type": 'application/json'
       }
     });
-    console.log("=XXXXXXXX======>", res)
+    console.log("=XXXXXXXX======>zzzzzz", res)
     const resProduct = await res.json();
     const _product = { product: value };
     setProduct(value);
