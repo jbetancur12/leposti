97e91e6f41a9e2c9023ca65aed0fb583be653d75
Jorge Betancur <jabetancur12@gmail.com>
2021-04-10 17:42:16 -0500

    TESTING COMFIRMATION

--- frontend/pages/test.js
+++ frontend/pages/test.js
@@ -1,7 +1,13 @@
-
+import { useEffect } from 'react'
+import React from 'reatc'
 
 export default function Index() {
-
+   useEffect(()=>{
+    addEventListener('fetch', function(event) {
+        // ServiceWorker intercepting a fetch
+        console.log(event.request.method)
+      });
+   },[])
     return (
         <div>HOLAAAAAAA</div>
     )
