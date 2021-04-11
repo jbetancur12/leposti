d1cd38ab2e2d8ad0bdbe6912f61f8dc9c46022e8
Jorge Betancur <jbetancur@elespectador.com>
2021-04-11 19:38:20 +0000

    Email Working

--- backend/api/order/services/order.js
+++ backend/api/order/services/order.js
@@ -1,5 +1,6 @@
 const { isDraft: draft } = require("strapi-utils").contentTypes;
 const _ = require("lodash");
+const dateFormat = require("dateformat");
 
 module.exports = {
   async update(params, data, { files } = {}) {
@@ -11,35 +12,19 @@ module.exports = {
       data,
       { isDraft }
     );
-    console.log(existingEntry);
+
     const entry = await strapi.query("order").update(params, validData);
+    // Create our number formatter.
+    const formatter = new Intl.NumberFormat("es-CO", {
+      style: "currency",
+      currency: "COP",
 
-    const emailTemplate = {
-      subject: "DETALLES DE SU COMPRA>",
-      text: `Welcome on mywebsite.fr!
-    Your account is now linked with:`,
-      html: `<p>Hola <%=dataUser.username%>,</p><p>aca estan los detalles de su compra.</p><p><br></p><p><strong style="color: rgb(51, 51, 51);">Referencia de Pedido:</strong><span style="color: rgb(119, 119, 119);">&nbsp;dkfndm&nbsp;</span></p><p><strong style="color: rgb(51, 51, 51);">Fecha compra:</strong><span style="color: rgb(119, 119, 119);">&nbsp;${existingEntry.update_at}&nbsp;</span></p><p><strong style="color: rgb(51, 51, 51);">Producto:</strong><span style="color: rgb(119, 119, 119);">&nbsp;${existingEntry.product.nombre}</span></p><p><strong style="color: rgb(51, 51, 51);">Fecha de Publicación:</strong><span style="color: rgb(119, 119, 119);">${existingEntry.fechaPublicacion}&nbsp;</span></p><p><strong style="color: rgb(51, 51, 51);">Valor $ ${existingEntry.total}&nbsp;</strong></p><p><strong style="color: rgb(51, 51, 51);">Medio de Pago :</strong><span style="color: rgb(119, 119, 119);">&nbsp;_______</span></p><p><span style="color: rgb(85, 84, 84);">&nbsp;</span></p><p><span style="color: rgb(85, 84, 84);">Usted puede revisar&nbsp;su Orden de publicación ingresando a su cuenta de cliente en&nbsp;</span><a href="http://www.leposti.com.co/" rel="noopener noreferrer" target="_blank" style="color: rgb(17, 85, 204);">www.leposti.com.co</a></p><p><span style="color: rgb(85, 84, 84);">Cualquier información adicional,&nbsp;&nbsp;comuníquese con servicio al cliente 3106503663, o travez del chat en el sitio</span></p>`,
-    };
+      // These options are needed to round to whole numbers if that's what you want.
+      //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+      //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
 
-    // await strapi.plugins.email.services.email.sendTemplatedEmail(
-    //   {
-    //     to: "alejobetancur12@hotmail.com",
-    //     from: "jabetancur12@gmail.com",
-    //   },
-    //   emailTemplate,
-    //   {
-    //     dataUser: {
-    //       username: existingEntry.user.username,
-    //       email: existingEntry.user.email,
-    //       referencia: existingEntry.referencia,
-    //       fechaCompra: existingEntry.updated_at,
-    //       producto: existingEntry.product.nombre,
-    //       fechaPublicacion: existingEntry.fechaPublicacion,
-    //       valor: existingEntry.total,
-    //       medioPago: "Online",
-    //     },
-    //   }
-    // );
+    /* $2,500.00 */
 
     try {
       await strapi.plugins["email-designer"].services.email.sendTemplatedEmail(
@@ -58,17 +43,21 @@ module.exports = {
             username: existingEntry.user.username,
             email: existingEntry.user.email,
             referencia: existingEntry.referencia,
-            fechaCompra: existingEntry.updated_at,
+            fechaCompra: existingEntry.updated_at.toLocaleString("es-CO", {
+              timeZone: "America/Bogota",
+            }),
             producto: existingEntry.product.nombre,
-            fechaPublicacion: existingEntry.fechaPublicacion,
-            valor: existingEntry.total,
+            fechaPublicacion: dateFormat(
+              existingEntry.fechaPublicacion,
+              "dd/mm/yyyy"
+            ),
+            valor: formatter.format(existingEntry.total),
             medioPago: "Online",
           },
         }
       );
     } catch (err) {
       strapi.log.debug("📺: ", err);
-      return ctx.badRequest(null, err);
     }
 
     return entry;
