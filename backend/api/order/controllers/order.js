bede32ec356ed37b277b3776cf845f41cf688fab
Jorge Betancur <jbetancur@elespectador.com>
2021-04-11 04:47:59 +0000

    Email Working

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -1,8 +1,31 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async update(ctx) {
+    const { id } = ctx.params;
+    console.log("===>", ctx.request.body);
+    let entity;
+
+    entity = await strapi.services.order.update({ id }, ctx.request.body);
+    // send an email by using the email plugin
+    await strapi.plugins["email"].services.email.send({
+      to: "alejobetancur12@hotmail.com",
+      from: "jabetancur12@gmail.com",
+      subject: "Comment posted that contains a bad words",
+      text: `
+          The comment #${entity.id} contain a bad words.
+
+          Comment:
+          ${entity.content}
+        `,
+    });
+
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
--- backend/api/order/services/order.js
+++ backend/api/order/services/order.js
@@ -1,8 +1,48 @@
-'use strict';
+const { isDraft: draft } = require("strapi-utils").contentTypes;
+const _ = require("lodash");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
- * to customize this service
- */
+module.exports = {
+  async update(params, data, { files } = {}) {
+    const existingEntry = await strapi.query("order").findOne(params);
 
-module.exports = {};
+    const isDraft = draft(existingEntry, strapi.models.order);
+    const validData = await strapi.entityValidator.validateEntityUpdate(
+      strapi.models.order,
+      data,
+      { isDraft }
+    );
+    console.log(existingEntry);
+    const entry = await strapi.query("order").update(params, validData);
+
+    const dataUser = {};
+
+    const emailTemplate = {
+      subject: "DETALLES DE SU COMPRA>",
+      text: `Welcome on mywebsite.fr!
+    Your account is now linked with:`,
+      html: `<p>Hola <%=dataUser.username%>,</p><p>aca estan los detalles de su compra.</p><p><br></p><p><strong style="color: rgb(51, 51, 51);">Referencia de Pedido:</strong><span style="color: rgb(119, 119, 119);">&nbsp;dkfndm&nbsp;</span></p><p><strong style="color: rgb(51, 51, 51);">Fecha compra:</strong><span style="color: rgb(119, 119, 119);">&nbsp;${existingEntry.update_at}&nbsp;</span></p><p><strong style="color: rgb(51, 51, 51);">Producto:</strong><span style="color: rgb(119, 119, 119);">&nbsp;${existingEntry.product.nombre}</span></p><p><strong style="color: rgb(51, 51, 51);">Fecha de Publicación:</strong><span style="color: rgb(119, 119, 119);">${existingEntry.fechaPublicacion}&nbsp;</span></p><p><strong style="color: rgb(51, 51, 51);">Valor $ ${existingEntry.total}&nbsp;</strong></p><p><strong style="color: rgb(51, 51, 51);">Medio de Pago :</strong><span style="color: rgb(119, 119, 119);">&nbsp;_______</span></p><p><span style="color: rgb(85, 84, 84);">&nbsp;</span></p><p><span style="color: rgb(85, 84, 84);">Usted puede revisar&nbsp;su Orden de publicación ingresando a su cuenta de cliente en&nbsp;</span><a href="http://www.leposti.com.co/" rel="noopener noreferrer" target="_blank" style="color: rgb(17, 85, 204);">www.leposti.com.co</a></p><p><span style="color: rgb(85, 84, 84);">Cualquier información adicional,&nbsp;&nbsp;comuníquese con servicio al cliente 3106503663, o travez del chat en el sitio</span></p>`,
+    };
+
+    await strapi.plugins.email.services.email.sendTemplatedEmail(
+      {
+        to: "alejobetancur12@hotmail.com",
+        from: "jabetancur12@gmail.com",
+      },
+      emailTemplate,
+      {
+        dataUser: {
+          username: existingEntry.user.username,
+          email: existingEntry.user.email,
+          referencia: existingEntry.referencia,
+          fechaCompra: existingEntry.updated_at,
+          producto: existingEntry.product.nombre,
+          fechaPublicacion: existingEntry.fechaPublicacion,
+          valor: existingEntry.total,
+          medioPago: "Online",
+        },
+      }
+    );
+
+    return entry;
+  },
+};
