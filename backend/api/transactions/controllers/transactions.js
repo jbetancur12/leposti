6df7cc58350dbef491cf2897aff7b70d1a4286f3
Jorge Betancur <jbetancur@elespectador.com>
2021-04-11 00:20:35 +0000

    Added controller transactions

--- backend/api/transactions/controllers/transactions.js
+++ backend/api/transactions/controllers/transactions.js
@@ -1,8 +1,25 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    const algo = await strapi.services.order.findOne({
+      referencia: ctx.request.body.reference_sale,
+    });
+    const response = ctx.request.body.state_pol;
+
+    const sendStatus = response === "1" ? "paid" : "unpaid";
+    const idOrder = algo.id;
+
+    await strapi.services.order.update({ id: idOrder }, { estado: sendStatus });
+
+    let entity;
+    entity = await strapi.services.transactions.create(ctx.request.body);
+    return sanitizeEntity(entity, { model: strapi.models.transactions });
+  },
+};
