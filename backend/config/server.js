c8629c538c0e86a5cf384937d8d5e48651090816
Jorge Betancur <jbetancur@elespectador.com>
2021-04-12 13:02:14 +0000

    Change url Env

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", "0.0.0.0"),
+  url: env("URL", "http://localhost"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "c19e82f11625a32f5dd9af23a072842b"),
