c13cef3040fc88fd2ad59e8269eb11bf86bc8c62
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2021-04-16 10:52:52 -0400

    Chat btn

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -261,15 +261,14 @@ const Home = () => {
             />
           </Col>
         </Row>
-        <div id='comm100-button-99d23bfd-f03f-4710-a185-0a95e15fdcb0'></div>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            var Comm100API=Comm100API||{};(function(t){function e(e){var a=document.createElement("script"),c=document.getElementsByTagName("script")[0];a.type="text/javascript",a.async=!0,a.src=e+t.site_id,c.parentNode.insertBefore(a,c)}t.chat_buttons=t.chat_buttons||[],t.chat_buttons.push({code_plan:"99d23bfd-f03f-4710-a185-0a95e15fdcb0",div_id:"comm100-button-99d23bfd-f03f-4710-a185-0a95e15fdcb0"}),t.site_id=10004261,t.main_code_plan="99d23bfd-f03f-4710-a185-0a95e15fdcb0",e("https://vue.comm100.com/livechat.ashx?siteId="),setTimeout(function(){t.loaded||e("https://standby.comm100vue.com/livechat.ashx?siteId=")},5e3)})(Comm100API||{})`,
-          }}
-        ></script>
       </Footer>
-      <UpBtn/>
+      <div id='comm100-button-99d23bfd-f03f-4710-a185-0a95e15fdcb0'></div>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          var Comm100API=Comm100API||{};(function(t){function e(e){var a=document.createElement("script"),c=document.getElementsByTagName("script")[0];a.type="text/javascript",a.async=!0,a.src=e+t.site_id,c.parentNode.insertBefore(a,c)}t.chat_buttons=t.chat_buttons||[],t.chat_buttons.push({code_plan:"99d23bfd-f03f-4710-a185-0a95e15fdcb0",div_id:"comm100-button-99d23bfd-f03f-4710-a185-0a95e15fdcb0"}),t.site_id=10004261,t.main_code_plan="99d23bfd-f03f-4710-a185-0a95e15fdcb0",e("https://vue.comm100.com/livechat.ashx?siteId="),setTimeout(function(){t.loaded||e("https://standby.comm100vue.com/livechat.ashx?siteId=")},5e3)})(Comm100API||{})`,
+        }}
+      ></script>
     </Layout>
   )
 }
@@ -872,8 +871,8 @@ export default function Index({ products }) {
               <Checkbox onChange={onChangeTerms}>
                 Acepto Terminos y condiciones
               </Checkbox>
-            </FormItem> */}
-              {/* <FormItem labelCol={{ span: 8 }} wrapperCol={{ span: 24 }}>
+            </FormItem> */
+              /* <FormItem labelCol={{ span: 8 }} wrapperCol={{ span: 24 }}>
               <Button
                 type='primary'
                 htmlType='submit'
@@ -908,7 +907,7 @@ export default function Index({ products }) {
       ></script>
     </>
   );
-}*/}
+}*/
 
 /*Index.getInitialProps = async (ctx) => {
   const res = await fetch(`https://api.leposti.ml/products`, {
