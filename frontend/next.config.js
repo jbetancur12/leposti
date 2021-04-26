cec0fb03620492d407e4e26321800798d316de86
Jorge Betancur <jabetancur12@gmail.com>
2021-04-26 21:13:37 +0000

    webpack 5

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,9 @@ const webpack = require('webpack');
 const { parsed: myEnv } = require('dotenv').config({});
 
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
