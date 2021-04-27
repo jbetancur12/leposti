114dbce816ce283fa69550649223b708195d34d6
Jorge Betancur <jabetancur12@gmail.com>
2021-04-27 00:09:17 +0000

    TEAST

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,4 +9,30 @@ module.exports = {
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
   },
+  "plugins": [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        "autoprefixer": {
+          "flexbox": "no-2009"
+        },
+        "stage": 3,
+        "features": {
+          "custom-properties": false
+        }
+      }
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}'
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ["html", "body"]
+      }
+    ],
+  ]
 };
