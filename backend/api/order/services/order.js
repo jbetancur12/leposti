0c3a5cd162dadb4b8a72dbe5bfd791ef0445a95e
Jorge Betancur <jabetancur12@gmail.com>
2021-04-28 12:14:47 +0000

    server

--- backend/api/order/services/order.js
+++ backend/api/order/services/order.js
@@ -18,10 +18,6 @@ module.exports = {
     const formatter = new Intl.NumberFormat("es-CO", {
       style: "currency",
       currency: "COP",
-
-      // These options are needed to round to whole numbers if that's what you want.
-      //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-      //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
     });
 
     /* $2,500.00 */
@@ -60,10 +56,8 @@ module.exports = {
             },
           }
         );
-      } catch (err) {
-        strapi.log.debug("📺: =>", err);
-      }
-      try {
+
+
         await strapi.plugins["email-designer"].services.email.sendTemplatedEmail(
           {
             to: entry.provider.email, // required
